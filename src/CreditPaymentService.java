49a06f7b8cd09a5c6563fd25ab7c42a35cadd3a9
Ilona Tatarenko <ilona-tatarenko@mail.ru>
2022-10-15 19:46:35 +0300

    Initial commit

--- src/CreditPaymentService.java
+++ src/CreditPaymentService.java
@@ -1,14 +1,22 @@
+import static java.lang.Math.pow;
+
 public class CreditPaymentService {
-
-
     public double calculate(int summa, int time) {
-        double percent = 9.99/100;
+        double percent = 9.99 / 100 / 12;
         double payment;
+        int timem = time * 12;
+        double kof;
+        double kkk;
+        double kk;
+        kk = 1 + percent;
+        kkk = pow(kk, timem);
+        kof = (percent * kkk) / (kkk - 1);
+        payment = kof * summa;
+        return payment;
+    }
 
-        double paymentAll = summa * percent;
-        double summaAll = paymentAll + summa;
-        payment = summaAll/time;
-
-
-    return (int) payment; }
 }
+
+
+
+
--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,11 @@
 public class Main {
     public static void main(String[] args) {
         CreditPaymentService service = new CreditPaymentService();
-        int payment = (int) service.calculate (1000000,12);
-        System.out.println(87911);
-
-        int payment1 = (int) service.calculate (1000000,24);
-        System.out.println(46140);
-
-        int payment2 = (int) service.calculate (1000000,36);
-        System.out.println(32262);
+        int payment = (int) service.calculate(1000000, 1);
+        System.out.println(payment);
+        int payment1 = (int) service.calculate(1000000, 2);
+        System.out.println(payment1);
+        int payment2 = (int) service.calculate(1000000, 3);
+        System.out.println(payment2);
     }
 }
