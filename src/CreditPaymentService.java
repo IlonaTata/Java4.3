b282047176cd1d6b9932db228015a4bec7184147
Ilona Tatarenko <ilona-tatarenko@mail.ru>
2022-10-26 08:51:06 +0300

    New Name

--- src/CreditPaymentService.java
+++ src/CreditPaymentService.java
@@ -5,13 +5,13 @@ public class CreditPaymentService {
         double percent = 9.99 / 100 / 12;
         double payment;
         int timem = time * 12;
-        double koffecient;
-        double partKoefficientVStepeni;
+        double koeffecient;
+        double partKoefficientInDegree;
         double partKoefficient;
         partKoefficient = 1 + percent;
-        partKoefficientVStepeni = pow(partKoefficient, timem);
-        koffecient = (percent * partKoefficientVStepeni) / (partKoefficientVStepeni - 1);
-        payment = koffecient * summa;
+        partKoefficientInDegree = pow(partKoefficient, timem);
+        koeffecient = (percent * partKoefficientInDegree) / (partKoefficientInDegree - 1);
+        payment = koeffecient * summa;
         return payment;
     }
 
